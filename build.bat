44949fe2afaffa13663fcb0da5a6cf883af101a8
artificial-lua <artificialluamail@gmail.com>
2024-11-20 08:32:23 +0900

    build 수정

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 go build -o dist/main.dll -buildmode=c-shared cmd/client/main.go
-go build -o dist/main.so -buildmode=c-shared cmd/server/main.go
+go build -o dist/main.so -buildmode=c-shared cmd/client/main.go
